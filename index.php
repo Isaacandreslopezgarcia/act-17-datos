acf625f8b377b78fb240d3e25b5c6fa0c81bc683
isaac0530 <>
2023-03-29 14:03:03 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+       $nombre = "hola me llamo isaac andres lopez garcia";
+       $calle = "calle 7";
+       $numero = "1789";
+       $colonia = "saragoza";
+       $municipio = "Saltillo coahuila";
+       $tel = "844 585 1639";
+
+       echo "¿como te llamas?: $nombre ";
+        ?>
+<?php
+       echo "¿en que calle vives? $calle ";
+        ?>
+<?php
+       echo "¿en que colonia vives? $colonia ";
+        ?>
+<?php
+       echo " su numero es $numero ";
+        ?>
+<?php
+       echo " es en el municipio de $municipio , ";
+       ?>
+<?php
+       echo " si es que te pierdes llamame y te indico : $tel ";
+    ?>
+  </body>
+</html>
