9667f845cccc6ca57501d72a705b521ffa157889
isaac0530 <125594906+Isaacandreslopezgarcia@users.noreply.github.com>
2023-03-29 14:20:43 +0000

    up date

--- index.php
+++ index.php
@@ -13,18 +13,23 @@
 
        echo "¿como te llamas?: $nombre ";
         ?>
+<br>
 <?php
        echo "¿en que calle vives? $calle ";
         ?>
+<br>
 <?php
        echo "¿en que colonia vives? $colonia ";
         ?>
+<br>
 <?php
        echo " su numero es $numero ";
         ?>
+<br>
 <?php
        echo " es en el municipio de $municipio , ";
        ?>
+<br>
 <?php
        echo " si es que te pierdes llamame y te indico : $tel ";
     ?>
